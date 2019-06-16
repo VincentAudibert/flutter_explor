ea9bb408dc279a3b2ecd3255afc23f918eb53fcc
VincentAudibert <vincent.audib@gmail.com>
2019-06-16 10:44:43 +0200

    Toyed, very beginning.

---
+++ lib/SessionEditScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class SessionEditScreen extends StatefulWidget {
+  @override
+  SessionEditState createState() => SessionEditState();
+
+}
+
+class SessionEditState extends State<SessionEditScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('A super edit screen !'),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,71 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_explor/SessionEditScreen.dart';
 
 void main() => runApp(MyApp());
 
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'Flutter Code Exploration';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: _title,
-      home: MyStatelessWidget(),
+      theme: ThemeData.dark(),
+      home: SessionListRoute(),
     );
   }
 }
 
 /// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+class SessionListRoute extends StatelessWidget {
+  SessionListRoute({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Ready, Set, Shop!'),
+        title: Text('Select a session'),
         actions: <Widget>[
           IconButton(
-            icon: Icon(Icons.shopping_cart),
-            tooltip: 'Open shopping cart',
+            icon: Icon(Icons.add),
+            tooltip: 'Create a new session',
             onPressed: () {
               // Implement navigation to shopping cart page here...
-              print('Shopping cart opened.');
+              print('clicked on create new session');
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (BuildContext context) {
+                  return SessionEditScreen();
+                })
+              );
             },
           ),
         ],
       ),
+      body: Center(
+        child: ListView(
+          children: <Widget>[
+            SessionListItem('first title'),
+            Text('session 2'),
+            SessionListItem('... last title ?')
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SessionListItem extends StatelessWidget {
+  String _title;
+  SessionListItem(this._title) : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(_title),
+      subtitle: Text('a subtitle'),
     );
   }
 }
