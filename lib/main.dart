b93a35f2eb62cf63d5fc83b85b1a87829b255758
VincentAudibert <vincent.audib@gmail.com>
2019-06-16 10:04:06 +0200

    Added ignored files.

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: MyStatelessWidget(),
+    );
+  }
+}
+
+/// This is the stateless widget that the main application instantiates.
+class MyStatelessWidget extends StatelessWidget {
+  MyStatelessWidget({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ready, Set, Shop!'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            tooltip: 'Open shopping cart',
+            onPressed: () {
+              // Implement navigation to shopping cart page here...
+              print('Shopping cart opened.');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
